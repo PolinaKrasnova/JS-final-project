21219830f48ef70da876943cb578c849e92f4c61
Polina Krasnova <polinakrasnaya3@gmail.com>
2018-09-05 04:40:42 +0700

    hello

---
+++ js/bundle/script.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+let tab = require('../parts/tab.js');
+let modal = require('../parts/modal.js');   
+let ajax = require('../parts/ajax.js');
+let slider = require('../parts/slider.js');
+let calc = require('../parts/calc.js');
+let timer = require('../parts/timer');
+
+tab();
+modal();
+ajax();
+slider();
+timer();
+calc();
+
+
+
+});
+
+
